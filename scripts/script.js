87e061e3568475eb29b485d70d9d6365a9defaf0
JesusLopz <20362487+JesusLopz@users.noreply.github.com>
2022-05-15 18:48:50 -0700

    added map

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+let map = L.map('map').setView([19.00981002976871, -98.5861632210551], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+document.getElementById('select-location').addEventListener('change',function(e){
+  let coords = e.target.value.split(",");
+  map.flyTo(coords,13);
+})
